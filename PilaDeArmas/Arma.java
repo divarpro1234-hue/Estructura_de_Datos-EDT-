b4501921ecaf0e030f87843046a2e98e758fd381
Divar Mamani <divarpro1234@gmail.com>
2026-03-27 22:04:57 -0400

    Agrego proyecto PilaDeArmas

---
+++ PilaDeArmas/Arma.java
@@ -0,0 +1,13 @@
+
+public class Arma {
+	private String serie;
+	private String nombre;
+	private int calibre;
+	private int peso;
+	private double precio;
+	
+	public void Arma() {
+		this.serie="fsfsdsdf";
+		this.nombre="sdfsf";
+	}
+}
