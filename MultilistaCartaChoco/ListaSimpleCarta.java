7cde8d3718f3c661e563b2dd1be60efd71759127
Divar Mamani <divarpro1234@gmail.com>
2026-03-27 22:04:45 -0400

    Agrego proyecto MultilistaCartaChoco

---
+++ MultilistaCartaChoco/ListaSimpleCarta.java
@@ -0,0 +1,18 @@
+
+public class ListaSimpleCarta {
+	protected NodoRem P;  //raiz de la lista
+	
+	public ListaSimpleCarta() {
+		P = null;		// la raiz apunta a nullo es decir
+						// se define una lista simple vacia
+	}
+
+	public NodoRem getP() {
+		return P;
+	}
+
+	public void setP(NodoRem p) {
+		P = p;
+	}
+
+}
---
+++ MultilistaCartaChoco/NodoRec.java
@@ -0,0 +1,30 @@
+
+public class NodoRec {
+	private int nroCel; 
+	private NodoRem sig;
+	
+	public NodoRec() {  //constructor
+		sig = null;
+	}
+
+	public int getNroCel() {
+		return nroCel;
+	}
+
+	public void setNroCel(int nroCel) {
+		this.nroCel = nroCel;
+	}
+
+	public NodoRem getSig() {
+		return sig;
+	}
+
+	public void setSig(NodoRem sig) {
+		this.sig = sig;
+	}
+
+	
+
+	
+	
+}
